eca437f7cbb78ff68e9450024530fc4f4e05e7fb
DiisK <diisk@hotmail.com>
2024-06-28 01:58:41 -0300

    added controller, views and static files

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProAuto.Data;
+using ProAuto.Interfaces.Repositories;
+using ProAuto.Interfaces.Services;
+using ProAuto.Repositories;
+using ProAuto.Services;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IAssociadoRepository, AssociadoRepository>();
+builder.Services.AddScoped<IVeiculoRepository, VeiculoRepository>();
+builder.Services.AddScoped<IAssociadoService, AssociadoService>();
+builder.Services.AddScoped<IVeiculoService, VeiculoService>();
 
 var app = builder.Build();
 
@@ -22,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Associado}/{action=Pesquisar}/{id?}");
 
 app.Run();
---
+++ wwwroot/js/mascaras.js
@@ -0,0 +1,54 @@
+﻿window.addEventListener("DOMContentLoaded", () => {
+  mascararCpf();
+  mascararTelefone();
+});
+
+function mascararCpf() {
+  [...document.getElementsByClassName("mascara-cpf")].forEach((input) => {
+    const mascarar = (input) => {
+      const text = input.value.replaceAll(/\D/g, "");
+      console.log("Teste",text)
+      let ret = "";
+      for (let i = 0; i < text.length; i++) {
+        if(i>10) break;
+        if ([3, 6].includes(i)) ret += ".";
+        if (i == 9) ret += "-";
+        ret += text[i];
+      }
+      input.value = ret;
+    };
+    input.addEventListener("keyup", () => {
+      mascarar(input);
+    });
+    input.addEventListener("change", () => {
+      mascarar(input);
+    });
+    mascarar(input);
+  });
+}
+
+function mascararTelefone() {
+  [...document.getElementsByClassName("mascara-telefone")].forEach((input) => {
+    const mascarar = (input) => {
+      const text = input.value.replaceAll(/\D/g, "");
+      if (text.length > 0) {
+        let ret = "(";
+        for (let i = 0; i < text.length; i++) {
+          if(i>10) break;
+          if (i == 2) ret += ")";
+          if ((i == 6 && text.length <= 10) || (i == 7 && text.length > 10))
+            ret += "-";
+          ret += text[i];
+        }
+        input.value = ret;
+      }
+    };
+    input.addEventListener("keyup", () => {
+      mascarar(input);
+    });
+    input.addEventListener("change", () => {
+      mascarar(input);
+    });
+    mascarar(input);
+  });
+}
